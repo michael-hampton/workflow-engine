5b6cf1d1a7261393c990c4d570bf9a14f7798d1a
michael-hampton <michael.hampton@kondor.co.uk>
2017-05-30 16:26:37 +0100

    Update Gateway.php

--- WorkflowClasses/Gateway.php
+++ WorkflowClasses/Gateway.php
@@ -38,5 +38,36 @@ class Gateway extends BaseGateway
             throw ($oError);
         }
     }
+    
+    public function updateStep($arrTrigger, $arrWorkflowObject)
+    {
+          $arrField = $this->objMysql->_select ("workflow.fields", array(), array("field_identifier" => trim ($arrTrigger['moveTo']['field'])));
+                    if ( empty ($arrField) )
+                    {
+                        throw new Exception ("Field cannot be found");
+                    }
+                    $strField = $arrField[0]['field_identifier'];
+                    $strValue = $objMike->arrElement[$strField];
+                    $conditionalValue = $arrTrigger['moveTo']['conditionValue'];
+                    $trueField = $arrTrigger['moveTo']['step_to'];
+                    $falseField = $arrTrigger['moveTo']['else'];
+                    switch ($arrTrigger['moveTo']['condition']) {
+                        case "=":
+                            if ( trim ($strValue) == trim ($conditionalValue) )
+                            {
+                                if ( isset ($arrWorkflowObject['elements'][$this->elementId]) )
+                                {
+                                    $arrWorkflowObject['elements'][$this->elementId]['current_step'] = $trueField;
+                                }
+                            }
+                            else
+                            {
+                                if ( isset ($arrWorkflowObject['elements'][$this->elementId]) )
+                                {
+                                    $arrWorkflowObject['elements'][$this->elementId]['current_step'] = $falseField;
+                                }
+                            }
+    
+    }
 
 }
