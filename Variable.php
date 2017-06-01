7b77ecc38e73a060cf504b19395a7c304db9e52d
michael-hampton <michael.hampton@kondor.co.uk>
2017-06-01 17:36:57 +0100

    Create Variable.php

---
+++ Variable.php
@@ -0,0 +1,41 @@
+class Variable extends  BaseVariable
+{
+    public function create()
+    {
+        try {
+            if($this->validate()) {
+                $this->save();
+            } else {
+                $msg = '';
+                foreach ($this->getValidationFailures() as $strMessage) {
+                    $msg .= $strMessage . "<br/>";
+                }
+                throw (new Exception( 'The row cannot be created! ' . $msg));
+            }
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+
+    public function update()
+    {
+        try {
+            if($this->validate()) {
+                $this->save();
+            } else {
+                $msg = '';
+                foreach ($this->getValidationFailures() as $strMessage) {
+                    $msg .= $strMessage . "<br/>";
+                }
+                throw (new Exception( 'The row cannot be created! ' . $msg));
+            }
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+
+    public function delete()
+    {
+        $this->delete();
+    }
+}
