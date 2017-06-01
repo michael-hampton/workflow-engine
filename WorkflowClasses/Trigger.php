e0b7461ea66ad0af9b2d8253037c53b2dc6e545c
michael-hampton <michael.hampton@kondor.co.uk>
2017-06-01 17:03:28 +0100

    Create Trigger.php

---
+++ WorkflowClasses/Trigger.php
@@ -0,0 +1,42 @@
+class Trigger extends BaseTrigger
+{
+/**
+     * Assign Trigger to a Step
+     *
+     * @param string $stepUid    Unique id of Step
+     * @param array  $aData  Data
+     *
+     * return array Data of the Trigger assigned to a Step
+     */
+    public function create ($stepUid, $aData)
+    {
+        try {
+            if ( !$this->stepExists ($stepUid) )
+            {
+                throw new Exception ("ID_STEP_DOES_NOT_EXIST");
+            }
+            $objects = $this->objMysql->_select ("workflow.status_mapping", array(), array("id" => $stepUid));
+            foreach ($objects as $row) {
+                if ( !empty ($row['step_trigger']) )
+                {
+                    $this->remove ($stepUid);
+                }
+            }
+            $this->setStepTo ($aData['step_to']);
+            $this->setWorkflowId ($aData['workflow_id']);
+            $this->setId ($stepUid);
+            $this->setTriggerType ($aData['trigger_type']);
+            if ( $this->validate () )
+            {
+                $result = $this->save ();
+                return $result;
+            }
+            else
+            {
+                throw (new Exception ("Failed Validation in class " . get_class ($this) . "."));
+            }
+        } catch (Exception $e) {
+            throw ($e);
+        }
+    }
+    }
