e61bf3cc898f4434a246a9588b4299d7cb323a05
michael-hampton <michael.hampton@kondor.co.uk>
2017-06-02 17:09:08 +0100

    Update WorkflowCollection.php

--- WorkflowClasses/WorkflowCollection.php
+++ WorkflowClasses/WorkflowCollection.php
@@ -1,25 +1,13 @@
 <?php
 
-class WorkflowCollection
+class WorkflowCollection extends BaseWorkflowCollection
 {
 
-    private $deptId;
-    private $name;
-    private $description;
-    private $requestId;
-    private $objMike;
-    private $parentId;
-    private $systemId;
+  
     private $objMysql;
-    private $arrValidationErrors;
-    private $new;
+  
     private $processCout = 0;
-    private $arrFieldMapping = array(
-        "description" => array("accessor" => "getDescription", "mutator" => "setDescription", "required" => false),
-        "request_type" => array("accessor" => "getName", "mutator" => "setName", "required" => true),
-        "system_id" => array("accessor" => "getSystemId", "mutator" => "setSystemId", "required" => true),
-        "dept_id" => array("accessor" => "getDeptId", "mutator" => "setDeptId", "required" => true)
-    );
+    p
     public $arrCollection = array();
 
     public function __construct ($intWorkflowCollectionId = null, $objMike = null)
@@ -39,24 +27,7 @@ class WorkflowCollection
         $this->objMike = $objMike;
     }
 
-    public function loadObject ($arrData)
-    {
-        foreach ($arrData as $formField => $formValue) {
-
-            if ( isset ($this->arrFieldMapping[$formField]) )
-            {
-                $mutator = $this->arrFieldMapping[$formField]['mutator'];
-
-                if ( method_exists ($this, $mutator) && is_callable (array($this, $mutator)) )
-                {
-                    if ( isset ($this->arrFieldMapping[$formField]) && trim ($formValue) != "" )
-                    {
-                        call_user_func (array($this, $mutator), $formValue);
-                    }
-                }
-            }
-        }
-    }
+  
 
     protected function getWorkflowCollectionData ($objMike)
     {
@@ -104,111 +75,7 @@ class WorkflowCollection
         return $workflowData;
     }
 
-    /**
-     * @return mixed
-     */
-    public function getDeptId ()
-    {
-        return $this->deptId;
-    }
-
-    /**
-     * @param mixed $deptId
-     */
-    public function setDeptId ($deptId)
-    {
-        $this->arrCollection['dept_id'] = $deptId;
-        $this->deptId = $deptId;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getName ()
-    {
-        return $this->name;
-    }
-
-    /**
-     * @param mixed $name
-     */
-    public function setName ($name)
-    {
-        $this->name = $name;
-        $this->arrCollection['request_type'] = $name;
-    }
-
-    public function getSystemId ()
-    {
-        return $this->systemId;
-    }
-
-    public function setSystemId ($systemId)
-    {
-        $this->systemId = $systemId;
-        $this->arrCollection['system_id'] = $systemId;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getDescription ()
-    {
-        return $this->description;
-    }
-
-    /**
-     * @param mixed $description
-     */
-    public function setDescription ($description)
-    {
-        $this->description = $description;
-        $this->arrCollection['description'] = $description;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getRequestId ()
-    {
-        return $this->requestId;
-    }
-
-    public function getNew ()
-    {
-        return $this->new;
-    }
-
-    public function setNew ($new)
-    {
-        $this->new = $new;
-    }
-
-    /**
-     * @param mixed $requestId
-     */
-    public function setRequestId ($requestId)
-    {
-        $this->requestId = $requestId;
-    }
-
-    /**
-     * 
-     * @return type
-     */
-    function getParentId ()
-    {
-        return $this->parentId;
-    }
-
-    /**
-     * 
-     * @param type $parentId
-     */
-    function setParentId ($parentId)
-    {
-        $this->parentId = $parentId;
-    }
+ 
     
     /**
      * 
@@ -269,40 +136,7 @@ class WorkflowCollection
         return $arrResult;
     }
 
-    public function validate ()
-    {
-        $errorCount = 0;
-
-        foreach ($this->arrFieldMapping as $fieldName => $arrField) {
-            if ( $arrField['required'] === true )
-            {
-                if ( !isset ($this->arrCollection[$fieldName]) || trim ($this->arrCollection[$fieldName]) == "" )
-                {
-                    $this->arrValidationErrors[] = $fieldName;
-                    $errorCount++;
-                }
-            }
-        }
-
-        if ( $errorCount > 0 )
-        {
-            return FALSE;
-        }
-
-        return TRUE;
-    }
-
-    public function save ()
-    {
-        if ( $this->new === true )
-        {
-            $this->objMysql->_insert ("workflow.request_types", $this->arrCollection);
-        }
-        else
-        {
-            
-        }
-    }
+    
 
     public function checkNameExists ($name)
     {
@@ -314,16 +148,6 @@ class WorkflowCollection
         }
 
         return false;
-    }
-
-    public function delete ()
-    {
-        if ( !is_numeric ($this->requestId) )
-        {
-            throw new Exception ("REQUEST ID HAS NOT BEEN SET");
-        }
-
-        $this->objMysql->_delete ("workflow.request_types", array("request_id" => $this->requestId));
     }
 
 }
