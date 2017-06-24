cc7d70fb9156c81e9075c0a51766005b810bd77c
michael-hampton <michael.hampton@kondor.co.uk>
2017-06-24 15:30:07 +0100

    create message relation

--- BusinessModel/MessageEventRelation.php
+++ BusinessModel/MessageEventRelation.php
@@ -13,6 +13,7 @@
  */
 class MessageEventRelation
 {
+    use Validator;
 
     private $objMysql;
 
@@ -95,10 +96,64 @@ class MessageEventRelation
         try {
             $objMessageEventRelations = new MessageDefinitions();
             return array(
-               $objMessageEventRelations->setPrjUid($record["MSGER_UID"]),
+                $objMessageEventRelations->setPrjUid ($record["MSGER_UID"]),
                 //$objMessageEventRelations $record["EVN_UID_THROW"],
                 $this->getFieldNameByFormatFieldName ("EVN_UID_CATCH") => $record["EVN_UID_CATCH"]
             );
+        } catch (\Exception $e) {
+            throw $e;
+        }
+    }
+
+    /**
+     * Create Message-Event-Relation for a Project
+     *
+     * @param string $projectUid Unique id of Project
+     * @param array  $arrayData  Data
+     *
+     * return array Return data of the new Message-Event-Relation created
+     */
+    public function create ($projectUid, array $arrayData)
+    {
+        try {
+            //Verify data
+            $this->throwExceptionIfDataIsNotArray ($arrayData, "\$arrayData");
+            $validator->throwExceptionIfDataIsEmpty ($arrayData, "\$arrayData");
+            //Set data
+            $arrayData = array_change_key_case ($arrayData, CASE_UPPER);
+            unset ($arrayData["MSGER_UID"]);
+            unset ($arrayData["PRJ_UID"]);
+            //Verify data
+            $process->throwExceptionIfNotExistsProcess ($projectUid, $this->arrayFieldNameForException["projectUid"]);
+            $this->throwExceptionIfDataIsInvalid ("", $projectUid, $arrayData);
+            //Create
+            $cnn = \Propel::getConnection ("workflow");
+            try {
+                $messageEventRelation = new \MessageEventRelation();
+                $messageEventRelationUid = \ProcessMaker\Util\Common::generateUID ();
+                $messageEventRelation->fromArray ($arrayData, \BasePeer::TYPE_FIELDNAME);
+                $messageEventRelation->setMsgerUid ($messageEventRelationUid);
+                $messageEventRelation->setPrjUid ($projectUid);
+                if ( $messageEventRelation->validate () )
+                {
+                    $cnn->begin ();
+                    $result = $messageEventRelation->save ();
+                    $cnn->commit ();
+                    //Return
+                    return $this->getMessageEventRelation ($messageEventRelationUid);
+                }
+                else
+                {
+                    $msg = "";
+                    foreach ($messageEventRelation->getValidationFailures () as $validationFailure) {
+                        $msg = $msg . (($msg != "") ? "\n" : "") . $validationFailure->getMessage ();
+                    }
+                    throw new \Exception (\G::LoadTranslation ("ID_RECORD_CANNOT_BE_CREATED") . (($msg != "") ? "\n" . $msg : ""));
+                }
+            } catch (\Exception $e) {
+                $cnn->rollback ();
+                throw $e;
+            }
         } catch (\Exception $e) {
             throw $e;
         }
