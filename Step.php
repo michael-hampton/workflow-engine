abc0f78bfc556826498af49aa21af599343491b8
michael-hampton <michael.hampton@kondor.co.uk>
2017-07-06 19:25:00 +0100

    Update Step.php

--- Step.php
+++ Step.php
@@ -1,7 +1,25 @@
 <?php
 class Step
 {
-
-
+ /*
+    * update the step information using an array with all values
+    * @param array $fields
+    * @return variant
+    */
+    public function update ($fields)
+    {
+        try {
+            $this->load( $fields['STEP_UID'] );
+            $this->loadObject( $fields );
+            if ($this->validate()) {
+                $result = $this->save();
+                return $result;
+            } else {
+                throw (new Exception( "Failed Validation in class " . get_class( $this ) . "." ));
+            }
+        } catch (Exception $e) {
+            throw ($e);
+        }
+    }
 
 }
