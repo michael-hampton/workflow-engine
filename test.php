5e60b7edee8d78cbc6f6ea50b97ee6515b17bf19
michael-hampton <michael.hampton@kondor.co.uk>
2017-07-14 17:27:36 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,166 +1,157 @@
-<?php
-
-ini_set ('memory_limit', '512M');
-$arraySystemConfiguration = array("debug" => 1, "DEBUG_SQL_LOG" => $_SERVER['DOCUMENT_ROOT'] . "/core/app/logs/debug.log");
-
-
-try {
-
-    if ( count ($argv) < 8 )
+ /**
+     * Validate data by field definition
+     *
+     * @param array $arrayData                  Data
+     * @param array $arrayFieldDefinition       Definition of fields
+     * @param array $arrayFieldNameForException Fields for exception messages
+     * @param bool  $flagValidateRequired       Validate required fields
+     *
+     * return void Throw exception if data has an invalid value
+     */
+    public function throwExceptionIfDataNotMetFieldDefinition($arrayData, $arrayFieldDefinition, $arrayFieldNameForException, $flagValidateRequired = true)
     {
+         try {
+            $arrayType1 = [
+                'int',
+                'integer',
+                'float',
+                'real',
+                'double',
+                'bool',
+                'boolean',
+                'string',
+                'date',
+                'hour',
+                'datetime'
+            ];
 
-        throw new Exception ('Error: Invalid number of arguments');
-    }
+            $arrayType2 = ['array', 'object'];
 
-    //Set variables
+            foreach ($arrayData as $key => $value) {
+                $fieldName = $key;
+                $fieldValue = $value;
 
-    $osIsLinux = strtoupper (substr (PHP_OS, 0, 3)) != 'WIN';
+                if (isset($arrayFieldDefinition[$fieldName])) {
 
-    $pathHome = $argv[1];
+                    $arrayFieldDefinition[$fieldName]["type"] = strtolower($arrayFieldDefinition[$fieldName]["type"]);
+                    $optionType = 0;
+                    $optionType = ($optionType == 0 && in_array($arrayFieldDefinition[$fieldName]["type"], $arrayType1)) ? 1 : $optionType;
+                    $optionType = ($optionType == 0 && in_array($arrayFieldDefinition[$fieldName]["type"], $arrayType2)) ? 2 : $optionType;
 
-    $pathTrunk = $argv[2];
+                    switch ($optionType) {
+                        case 1:
 
-    $pathOutTrunk = $argv[3];
+                            //empty
+                            if ($arrayFieldDefinition[$fieldName]['required'] === 'true' && trim($fieldValue) == '') {
+                                throw new \Exception('ID_INVALID_VALUE_CAN_NOT_BE_EMPTY');
+                            }
 
-    $cronName = $argv[4];
+                            //defaultValues
+                            if (isset($arrayFieldDefinition[$fieldName]['defaultValues']) &&
+                                !empty($arrayFieldDefinition[$fieldName]['defaultValues']) &&
+                                !in_array($fieldValue, $arrayFieldDefinition[$fieldName]['defaultValues'], true)
+                            ) {
+                                throw new \Exception('ID_INVALID_VALUE_ONLY_ACCEPTS_VALUES');
+                            }
 
-    $workspace = $argv[5];
+                            $regexpDate = '[0-9]{2}[-|\/]{1}[0-9]{2}[-|\/]{1}[0-9]{4}';
+                            $regexpTime = '(?:[0-1]\d|2[0-3])\:[0-5]\d\:[0-5]\d';
 
-    $dateSystem = $argv[6];
+                            $regexpDatetime = $regexpDate . '\s' . $regexpTime;
 
-    $sNow = $argv[7]; //$date
+                            switch ($arrayFieldDefinition[$fieldName]["type"]) {
+                                case "date":
+                                    if (!preg_match("/^" . $regexpDate . "$/", $fieldValue)) {
+                                        throw new \Exception('ID_INVALID_VALUE');
+                                    }
+                                    break;
+                                case "hour":
+                                    $regexpTime = '/^' . $regexpTime . '$/';
 
-    /*     * ********************* Set constants ************************* */
+                                    if (array_key_exists('regexp', $arrayFieldDefinition[$fieldName])) {
+                                        $regexpTime = $arrayFieldDefinition[$fieldName]['regexp'];
+                                    }
 
-    define ('PATH_SEP', ($osIsLinux) ? '/' : '\\');
-
-    define ('PATH_HOME', $pathHome);
-
-    define ('PATH_TRUNK', $pathTrunk);
-
-    define ('PATH_OUTTRUNK', $pathOutTrunk);
-
-    define ('PATH_CLASSES', PATH_HOME . PATH_SEP . 'app' . PATH_SEP . 'library' . PATH_SEP);
-
-    define ('SYS_LANG', 'en');
-
-    $e_all = (defined ('E_DEPRECATED')) ? E_ALL & ~E_DEPRECATED : E_ALL;
-
-    $e_all = (defined ('E_STRICT')) ? $e_all & ~E_STRICT : $e_all;
-
-    $e_all = ($arraySystemConfiguration['debug']) ? $e_all : $e_all & ~E_NOTICE;
-
-
-    /*     * *************** Load Classes **************** */
-    define ("HOME_DIR", PATH_HOME);
-    define ("DEBUG_LOCATION", HOME_DIR . "/core/app/logs/debug.log");
-
-    require_once 'Persistent.php';
-    require_once 'config.php';
-    require_once 'registry.php';
-    require_once "Mysql.php";
-     require_once 'BPMN/Flow.php';
-    require_once 'BPMN/Task.php';
-
-    require_once 'BusinessModel/Validator.php';
-    require_once 'UserClasses/BaseUser.php';
-    require_once 'UserClasses/Users.php';
-    require_once 'Process/BaseProcess.php';
-    require_once 'BusinessModel/Process.php';
-    require_once 'WorkflowClasses/Save.php';
-    require_once 'BusinessModel/MessageEventRelation.php';
-    require_once 'BusinessModel/MessageEventDefinition.php';
-    require_once 'BusinessModel/MessageApplication.php';
-    require_once 'BusinessModel/Event.php';
-    require_once 'WorkflowClasses/Elements.php';
-    require_once 'BusinessModel/StepGateway.php';
-    require_once 'WorkflowClasses/WorkflowStep.php';
-    require_once 'BusinessModel/FieldFactory.php';
-    require_once 'BaseVariable.php';
-    require_once 'Variable.php';
-    require_once 'BusinessModel/StepVariable.php';
-    require_once 'BusinessModel/StepPermissions.php';
-    require_once 'BusinessModel/ProcessSupervisor.php';
-    require_once 'BusinessModel/StepTrigger.php';
-    require_once 'Notifications/BaseNotification.php';
-    require_once 'Notifications/Notifications.php';
-    require_once 'Notifications/SendNotification.php';
-    require_once 'Fields/FieldValidator.php';
-    require_once 'WorkflowClasses/Workflow.php';
-    require_once 'BusinessModel/UsersFactory.php';
-
-    require_once 'BusinessModel/Cases.php';
-    require_once 'Event/BaseTimerEvent.php';
-    require_once 'Event/TimerEvent.php';
-    require_once 'BusinessModel/TimerEvent.php';
-
-    if ( session_id () === "" )
-    {
-        session_start ();
-    }
-
-    $_SESSION['user']['usrid'] = 2;
-
-    // set error reporting
-    //Do not change any of these settings directly, use env.ini instead
-
-    ini_set ('display_errors', $arraySystemConfiguration['debug']);
-
-    ini_set ('error_reporting', $e_all);
-
-    ini_set ('short_open_tag', 'On');
-
-    ini_set ('default_charset', 'UTF-8');
-
-    $argvx = '';
+                                    if (!preg_match($regexpTime, $fieldValue)) {
+                                        throw new \Exception('ID_INVALID_VALUE');
+                                    }
+                                    break;
+                                case "datetime":
+                                    if (!preg_match("/^" . $regexpDatetime . "$/", $fieldValue)) {
+                                        throw new \Exception('ID_INVALID_VALUE');
+                                    }
+                                    break;
+                            }
 
 
-    // build arguments
-    for ($i = 8; $i <= count ($argv) - 1; $i++) {
+                            break;
 
-        /* ----------------------------------********--------------------------------- */
+                        case 2:
+                            switch ($arrayFieldDefinition[$fieldName]["type"]) {
+                                case "array":
+                                    $regexpArray1 = "\s*array\s*\(";
+                                    $regexpArray2 = "\)\s*";
 
-        $argvx = $argvx . (($argvx != '') ? ' ' : '') . $argv[$i];
+                                    //type
+                                    if (!is_array($fieldValue)) {
+                                        if ($fieldValue != "" && !preg_match("/^" . $regexpArray1 . ".*" . $regexpArray2 . "$/", $fieldValue)) {
+                                            throw new \Exception('ID_INVALID_VALUE_THIS_MUST_BE_ARRAY');
+                                        }
+                                    }
 
-        /* ----------------------------------********--------------------------------- */
-    }
+                                    //empty
+                                    if (!$arrayFieldDefinition[$fieldName]["empty"]) {
+                                        $arrayAux = array();
 
-    // run cron
-    try {
+                                        if (is_array($fieldValue)) {
+                                            $arrayAux = $fieldValue;
+                                        }
 
-        switch ($cronName) {
+                                        if (is_string($fieldValue) && trim($fieldValue) != '') {
+                                            //eval("\$arrayAux = $fieldValue;");
 
-            case 'messageeventcron':
+                                            if (preg_match("/^" . $regexpArray1 . "(.*)" . $regexpArray2 . "$/", $fieldValue, $arrayMatch)) {
+                                                if (trim($arrayMatch[1], " ,") != "") {
+                                                    $arrayAux = [0];
+                                                }
+                                            }
+                                        }
 
-                $messageApplication = new \BusinessModel\MessageApplication();
+                                        if (empty($arrayAux)) {
+                                            throw new \Exception('ID_INVALID_VALUE_CAN_NOT_BE_EMPTY');
+                                        }
+                                    }
 
+                                    //defaultValues
+                                    if (isset($arrayFieldDefinition[$fieldName]['defaultValues']) &&
+                                        !empty($arrayFieldDefinition[$fieldName]['defaultValues'])
+                                    ) {
+                                        $arrayAux = [];
 
+                                        if (is_array($fieldValue)) {
+                                            $arrayAux = $fieldValue;
+                                        }
 
-                $messageApplication->catchMessageEvent (true);
+                                        if (is_string($fieldValue) && trim($fieldValue) != '') {
+                                            eval("\$arrayAux = $fieldValue;");
+                                        }
 
-                break;
+                                        foreach ($arrayAux as $value) {
+                                            if (!in_array($value, $arrayFieldDefinition[$fieldName]["defaultValues"], true)) {
+                                                throw new \Exception('ID_INVALID_VALUE_ONLY_ACCEPTS_VALUES');
+                                            }
+                                        }
+                                    }
+                                    break;
+                            }
+                            break;
+                    }
 
-            //php .\test.php C:\xampp\htdocs mike mike timereventcron mike mike mike
-            case 'timereventcron':
+                    echo $optionType;
+                }
 
-                $timerEvent = new \BusinessModel\TimerEvent();
-
-                $timerEvent->startContinueCaseByTimerEvent (date ('Y-m-d H:i:s'), true);
-
-                break;
+            }
+        } catch(Exception $e) {
+            throw $e;
         }
-    } catch (Exception $e) {
-
-        echo $e->getMessage () . "\n";
-
-
-
-        eprintln ('Problem in workspace: ' . $workspace . ' it was omitted.', 'red');
     }
-
-
-    //eprintln();
-} catch (Exception $ex) {
-    
-}
-
