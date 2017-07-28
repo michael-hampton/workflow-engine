6b6debd8afde630ec9f097ef9b6bb497cf717eef
michael-hampton <michael.hampton@kondor.co.uk>
2017-07-28 17:21:56 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,281 +1,229 @@
 <?php
-namespace ProcessMaker\BusinessModel;
 
-class EmailEvent
-{
-    use Validator;
+    class FileLoggerException extends Exception {}
     
-    private $objMysql;
-
     /**
-     * Get the Email-Event data
-     * @var string $evn_uid. uid for activity
-     * @var string $pro_uid. uid for process
-     * return array
+     * File logger
+     * 
+     * Log notices, warnings, errors or fatal errors into a log file.
+     * 
+     * @author gehaxelt
      */
-    public function getEmailEventData($stepId)
-    {
-        try {
-            //Get data
-            $criteria = $this->getEmailEventCriteria();
-            
-            $criteria .= " WHERE triggering_status = ?";
-            
-            $row = $this->objMysql->_query($criteria, [$stepId]);
-
-            return $row;
-        } catch (\Exception $e) {
-            throw $e;
-        }
-    }
-
-    /**
-     * Get the Email-Event data
-     * @var string $emailEventUid. uid for email event
-     * @var string $pro_uid. uid for process
-     * return array
-     */
-    public function getEmailEventDataByUid($stepId, $emailEventUid)
-    {
-        try {
-            //Get data
-            $criteria = $this->getEmailEventCriteria();
-            
-            $criteria .= " WHERE id = ? AND triggering_status = ?";
-            
-            $row = $this->objMysql->_query($criteria, [$emailEventUid, $stepId]);
-           
-            return $row;
-        } catch (\Exception $e) {
-            throw $e;
-        }
-    }
-
-    /**
-     * Save Data for Email-Event
-     * @var string $prj_uid. Uid for Process
-     * @var string $arrayData. Data for Trigger
-     *
-     * return array
-     */
-    public function save($stepId, $arrayData = array())
-    {
-        try {
-            //Verify data
-
-            $this->throwExceptionIfDataIsNotArray($arrayData, "\$arrayData");
-            $this->throwExceptionIfDataIsEmpty($arrayData, "\$arrayData");
-
-            //Verify data
-            // verify step
-
-            //Create
-
-            try {
-                $emailEvent = new \EmailEvent();
-
-                $emailEvent->loadObject($arrayData);
-
-                $emailEvent->setPrjUid($prj_uid);
-
-                $emailEventUid = $emailEvent->save();
-
-                return $this->getEmailEvent($emailEventUid);
-            } catch (\Exception $e) {
-                throw $e;
-            }
-        } catch (\Exception $e) {
-            throw $e;
-        }
-    }
-
-    /**
-     * Update Email-Event
-     *
-     * @param string $emailEventUid Unique id of Email-Event
-     * @param array  $arrayData Data
-     *
-     * return array Return data of the Email-Event updated
-     */
-    public function update($emailEventUid, array $arrayData)
-    {
-        try {
-            //Verify data
-            $this->throwExceptionIfDataIsNotArray($arrayData, "\$arrayData");
-            $this->throwExceptionIfDataIsEmpty($arrayData, "\$arrayData");
-
-            //Set variables
-            $arrayEmailEventData = $this->getEmailEvent($emailEventUid);
-
-            //Verify data
-            $this->verifyIfEmailEventExists($emailEventUid);
-
-            //Update
-
-            try {
-                $emailEvent = $this->retrieveByPK($emailEventUid);
-                $emailEvent->loadObject($arrayData);
-
-                $result = $emailEvent->save();
-
-                return $result;
-
-            } catch (\Exception $e) {
-
-                throw $e;
-            }
-        } catch (\Exception $e) {
-            throw $e;
-        }
-    }
-    
-    public function retrieveByPK($pk)
-    {
-        $result = $this->objMysql->_select("task_manager.auto_notifications", [], ["id" => $pk]);
+    class Log {
         
-        if(!isset($result[0]) || empty($result[0])) {
-            return false;
-        }
-    }
-
-    /**
-     * Delete Email-Event
-     *
-     * @param string $emailEventUid Unique id of Email-Event
-     *
-     * return void
-     */
-    public function delete($pro_uid, $emailEventUid, $passValidation = true)
-    {
-        try {
-            //Verify data
-            if ($passValidation) {
-                $this->verifyIfEmailEventExists($emailEventUid);
-
+        /**
+         * Holds the file handle.
+         * 
+         * @var resource
+         */
+        protected $fileHandle = NULL;
+        
+        /**
+         * The time format to show in the log.
+         * 
+         * @var string
+         */
+        protected $timeFormat = 'd.m.Y - H:i:s';
+        
+        /**
+         * The file permissions.
+         */
+        const FILE_CHMOD = 756;
+        
+        const NOTICE = '[NOTICE]';
+        const WARNING = '[WARNING]';
+        const ERROR = '[ERROR]';
+        const FATAL = '[FATAL]';
+        
+        /**
+         * Opens the file handle.
+         * 
+         * @param string $logfile The path to the loggable file.
+         */
+        public function __construct($logfile) {
+            if($this->fileHandle == NULL){
+                $this->openLogFile($logfile);
             }
-            //Delete Email event
-
-
-        } catch (\Exception $e) {
-            throw $e;
         }
-    }
-
-    /**
-     * Delete Email-Event by event uid
-     *
-     * @param string $emailEventUid Unique id of Email-Event
-     *
-     * return void
-     */
-    public function deleteByEvent($stepId)
-    {
-        try {
-            //Verify data
-            if (!$this->existsEvent($prj_uid, $evn_uid)) {
-                throw new \Exception(\G::LoadTranslation("ID_EMAIL_EVENT_DEFINITION_DOES_NOT_EXIST"));
-            }
-            $arrayData = $this->existsEvent($prj_uid, $evn_uid);
-            $this->delete($prj_uid, $arrayData[0]);
-
-        } catch (\Exception $e) {
-            throw $e;
+        
+        /**
+         * Closes the file handle.
+         */
+        public function __destruct() {
+            $this->closeLogFile();
         }
-    }
-
-    /**
-     * Get data of a Email-Event
-     *
-     * @param string $emailEventUid Unique id of Email-Event
-     * @param bool   $flagGetRecord             Value that set the getting
-     *
-     * return array Return an array with data of a Email-Event
-     */
-    public function getEmailEvent($emailEventUid)
-    {
-        try {
-            $result = $this->objMysql->_select("task_manager.auto_notifications", [], ["id" => $emailEventUid]);
-            
-            if(!isset($result[0]) || empty($result[0])) {
-                return false;
-            }
-
-            //Return
-            return $result;
-        } catch (\Exception $e) {
-            throw $e;
-        }
-    }
-
-    /**
-     * Verify if exists the Email-Event
-     *
-     * @param string $emailEventUid Unique id of Email-Event
-     *
-     * return bool Return true if exists the Email-Event, false otherwise
-     */
-    public function exists($emailEventUid)
-    {
-        try {
-            $obj = $this->retrieveByPK($emailEventUid);
-
-            return (!is_null($obj))? true : false;
-        } catch (\Exception $e) {
-            throw $e;
-        }
-    }
-
-    /**
-     * Get criteria for Email-Event
-     *
-     * return object
-     */
-    public function getEmailEventCriteria()
-    {
-        try {
-         $criteria = "SELECT * FROM task_manager.auto_notifications ";
-
-            return $criteria;
-        } catch (\Exception $e) {
-            throw $e;
-        }
-    }
-
-
-    public function verifyIfEmailEventExists($emailEventUid)
-    {
-        if (!$this->exists($emailEventUid)) {
-            throw new \Exception("ID_EMAIL_EVENT_DEFINITION_DOES_NOT_EXIST");
-        }
-    }
-
-    /**
-     * Verify if exists the Event of a Message-Event-Definition
-     *
-     * @param string $projectUid                         Unique id of Project
-     * @param string $eventUid                           Unique id of Event
-     *
-     * return bool Return true if exists the Event of a Message-Event-Definition, false otherwise
-     */
-    public function existsEvent($projectUid, $eventUid)
-    {
-        try {
-          
-             //Get data
-            $criteria = $this->getEmailEventCriteria();
-            
-            $criteria .= " WHERE triggering_status = ?";
-            
-            $row = $this->objMysql->_query($criteria, [$stepId]);
-            
-            if(!isset($row[0]) || empty($row[0])){
-                return false;
+        
+        /**
+         * Logs the message into the log file.
+         * 
+         * @param  string $message     The log message.
+         * @param  int    $messageType Optional: urgency of the message.
+         */
+        public function log($message, $messageType = Log::WARNING) {
+            if($this->fileHandle == NULL){
+                throw new FileLoggerException('Logfile is not opened.');
             }
             
-            return $row;
-
-        } catch (\Exception $e) {
-            throw $e;
+            if(is_array($message)) {
+                $string = "";
+                
+                foreach($message as $key => $value) {
+                    if(is_array($value)) {
+                        /* TODO */
+  
+                    } else {
+                        $string .=  "\t {".$key.': '.$value."}\n ";
+                    }
+                }
+                
+                $message = $string;
+            }
+            
+            if($messageType != Log::NOTICE &&
+               $messageType != Log::WARNING &&
+               $messageType != Log::ERROR &&
+               $messageType != Log::FATAL
+            ){
+                throw new FileLoggerException('Wrong $messagetype given.');
+            }
+            
+            $this->writeToLogFile("[".$this->getTime()."]".$messageType." - ".$message);
         }
+        
+        /**
+         * Writes content to the log file.
+         * 
+         * @param string $message
+         */
+        private function writeToLogFile($message) {
+            
+            try {
+                 flock($this->fileHandle, LOCK_EX);
+            fwrite($this->fileHandle, $message.PHP_EOL);
+            flock($this->fileHandle, LOCK_UN);
+            } catch(Exception $e) {
+                throw $e;
+            }
+        }
+        
+        /**
+         * Returns the current timestamp.
+         * 
+         * @return string with the current date
+         */
+        private function getTime() {
+            return date($this->timeFormat);
+        }
+        
+        /**
+         * Closes the current log file.
+         */
+        protected function closeLogFile() {
+            if($this->fileHandle != NULL) {
+                fclose($this->fileHandle);
+                $this->fileHandle = NULL;
+            }
+        }
+        
+        /**
+         * Opens a file handle.
+         * 
+         * @param string $logFile Path to log file.
+         */
+        public function openLogFile($logFile) {
+            $this->closeLogFile();
+            
+            if(!is_dir(dirname($logFile))){
+                if(!mkdir(dirname($logFile), Log::FILE_CHMOD, true)){
+                    throw new FileLoggerException('Could not find or create directory for log file.');
+                }
+            }
+            
+            if(!$this->fileHandle = fopen($logFile, 'a+')){
+                throw new FileLoggerException('Could not open file handle.');
+            }
+        }
+        
     }
-}
+
+  (new Log($_SERVER['DOCUMENT_ROOT']."v3/app/logs/test.log"))->log(
+                                                                         array(
+                                                                               'mike 1' => "test 0",
+                                                                               'lexi 2' => "test 1",
+                                                                               'uan 3' => "test 2",
+                                                                               'bish 5' => "test 3",
+                                                                               'paul 6' => "test 4"
+                                                                               ), Log::NOTICE);
+
+
+
+
+ $blIsParralelTask = true;
+        $blTaskUsersCompleted = 0;
+
+        $this->completeAuditObject($arrCompleteData, $blIsParralelTask);
+        
+        if($blIsParralelTask === true) {
+
+            foreach($this->objAudit['steps'][$this->_workflowStepId]['parallelUsers'] as $key => $parallelUser) {
+                if(isset($parallelUser['date_completed']) && trim($parallelUser['date_completed']) !== "") {
+                    $blTaskUsersCompleted++;
+                }
+            }
+            
+            if(count($this->objAudit['steps'][$this->_workflowStepId]['parallelUsers']) !== $blTaskUsersCompleted) {
+                $this->objWorkflow['current_step'] = $this->_workflowStepId;
+            }
+        }
+
+
+
+
+ public function getParallelUsers()
+    {
+        $arrUsers = array("michael.hampton", "admin", "lexi.hampton");
+        
+        return $arrUsers;
+    }
+
+    /**
+     * @param array $arrCompleteData
+     * @return bool
+     */
+    private function completeAuditObject(array $arrCompleteData = [], $isParallel = false)
+    {
+        if($isParallel === true && !isset($this->objAudit['steps'][$this->_workflowStepId]['parallelUsers'])) {
+            
+            $arrUsers = $this->getParallelUsers();
+            
+            $parallelUsers = [];
+            
+            foreach($arrUsers as $key => $user) {
+                $parallelUsers[$key]['username'] = $user;
+            }
+            
+            $this->objAudit['steps'][$this->_workflowStepId]['parallelUsers'] = $parallelUsers;
+        }
+        
+        $this->objAudit['steps'][$this->_workflowStepId]['step'] = $this->_workflowStepId;
+        $this->objAudit['steps'][$this->_workflowStepId]['dateCompleted'] = date("Y-m-d H:i:s");
+
+        if (!empty($arrCompleteData)) {
+            $this->objAudit['steps'][$this->_workflowStepId]['status'] = $arrCompleteData['status'];
+            $this->objAudit['steps'][$this->_workflowStepId]['completed_by'] = $arrCompleteData['completed_by'];
+            
+            if($isParallel === true && isset($this->objAudit['steps'][$this->_workflowStepId]['parallelUsers'])) {
+                if(isset($arrCompleteData['completed_by']) && trim($arrCompleteData['completed_by']) !== "") {
+                    foreach($this->objAudit['steps'][$this->_workflowStepId]['parallelUsers'] as $key => $parallelUser) {
+                       if(trim($parallelUser['username']) === trim($arrCompleteData['completed_by'])) {
+                        $this->objAudit['steps'][$this->_workflowStepId]['parallelUsers'][$key]['date_completed'] = date("Y-m-d H:i:s");
+                       }
+                    }
+                }
+            }
+
+        }
+
+        return true;
+    }
+    
