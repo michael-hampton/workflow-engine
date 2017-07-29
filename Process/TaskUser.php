7c6c2918c13c83977d88da9d53093931ae84d3e8
michael-hampton <michael.hampton@kondor.co.uk>
2017-07-29 21:20:24 +0100

    Update TaskUser.php

--- Process/TaskUser.php
+++ Process/TaskUser.php
@@ -120,17 +120,14 @@ class TaskUser extends BaseTaskUser
 
     public function getCountAllTaksByGroups ()
     {
-        $oCriteria = new Criteria ('workflow');
-        $oCriteria->addAsColumn ('GRP_UID', TaskUserPeer::USR_UID);
-        $oCriteria->addSelectColumn ('COUNT(*) AS CNT');
-        $oCriteria->add (TaskUserPeer::TU_TYPE, 1);
-        $oCriteria->add (TaskUserPeer::TU_RELATION, 2);
-        $oCriteria->addGroupByColumn (TaskUserPeer::USR_UID);
-        $oDataset = TaskUserPeer::doSelectRS ($oCriteria);
-        $oDataset->setFetchmode (ResultSet::FETCHMODE_ASSOC);
-        $aRows = Array();
-        while ($oDataset->next ()) {
-            $row = $oDataset->getRow ();
+        $sql = "SELECT COUNT(*) AS CNT, USR_UID AS GRP_UID FROM workflow.TASK_USER 
+        WHERE TU_TYPE = 1 AND TU_RELATION = 2
+        GROUP BY USR_UID";
+        
+        $results = $this->objMysql->_query($sql);
+        $aRows = array();
+        
+        foreach($results as $row){
             $aRows[$row['GRP_UID']] = $row['CNT'];
         }
         return $aRows;
@@ -143,26 +140,18 @@ class TaskUser extends BaseTaskUser
         $groupsTask = array();
         $usersTask = array();
         //getting task's users
-        $criteria = new Criteria ('workflow');
-        $criteria->addSelectColumn (UsersPeer::USR_FIRSTNAME);
-        $criteria->addSelectColumn (UsersPeer::USR_LASTNAME);
-        $criteria->addSelectColumn (UsersPeer::USR_USERNAME);
-        $criteria->addSelectColumn (TaskUserPeer::TAS_UID);
-        $criteria->addSelectColumn (TaskUserPeer::USR_UID);
-        $criteria->addSelectColumn (TaskUserPeer::TU_TYPE);
-        $criteria->addSelectColumn (TaskUserPeer::TU_RELATION);
-        $criteria->addJoin (TaskUserPeer::USR_UID, UsersPeer::USR_UID, Criteria::LEFT_JOIN);
-        $criteria->add (TaskUserPeer::TAS_UID, $TAS_UID);
-        $criteria->add (TaskUserPeer::TU_TYPE, $TU_TYPE);
-        $criteria->add (TaskUserPeer::TU_RELATION, 1);
-        $dataset = TaskUserPeer::doSelectRS ($criteria);
-        $dataset->setFetchmode (ResultSet::FETCHMODE_ASSOC);
-        while ($dataset->next ()) {
-            $usersTask[] = $dataset->getRow ();
-        }
+        
+        $sql = "SELECT firstName, lastName, USR_UID, username, TAS_UID, TU_TYPE, TU_RELATION
+        FROM user_management.poms_users u
+        LEFT JOIN workflow.TASK_USER tu ON tu.USR_UID = u.usrid
+        WHERE TAS_UID = ? AND TU_TYPE = ? AND TU_RELATION = 1";
+        
+        $arrParameters = array($TAS_UID, $TU_TYPE);
+       
+        $usersTask = $this->objMysql->_query($sql);
+        
         //getting task's groups
-        $delimiter = DBAdapter::getStringDelimiter ();
-        $criteria = new Criteria ('workflow');
+        
         $criteria->addAsColumn ('GRP_TITLE', 'CONTENT.CON_VALUE');
         $criteria->addSelectColumn (TaskUserPeer::TAS_UID);
         $criteria->addSelectColumn (TaskUserPeer::USR_UID);
