7a0bda3c86a9db954bb2966c17ea850014390098
michael-hampton <michael.hampton@kondor.co.uk>
2017-09-03 11:46:08 +0100

    Tidy Up

--- BusinessModel/FormBuilder.php
+++ BusinessModel/FormBuilder.php
@@ -215,15 +215,21 @@ class FormBuilder
                     /* If the appropriate class has a "set" method for the property provided, then
                       it is called instead or setting the property directly. */
                     if ( isset ($method_reference["set" . $property]) )
+                    {
                         $this->$method_reference["set" . $property] ($value);
+                    }
                     elseif ( isset ($property_reference[$property]) )
+                    {
                         $this->$property_reference[$property] = $value;
 
-                    /* Entries that don't match an available class property are stored in the attributes
-                      property if applicable.  Typically, these entries will be element attributes such as
-                      class, value, onkeyup, etc. */
+                        /* Entries that don't match an available class property are stored in the attributes
+                          property if applicable.  Typically, these entries will be element attributes such as
+                          class, value, onkeyup, etc. */
+                    }
                     else
+                    {
                         $this->setAttribute ($property, $value);
+                    }
                 }
             }
         }
