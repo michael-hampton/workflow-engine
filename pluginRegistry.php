32be1cc51b8c2cf56b99fc2161502bd8ca6d4e84
michael-hampton <michael.hampton@kondor.co.uk>
2017-09-26 19:10:10 +0100

    Update pluginRegistry.php

--- pluginRegistry.php
+++ pluginRegistry.php
@@ -1,12 +1,25 @@
 <?php
 class pluginRegistry
 {
+    
+    private $objTask;
+    private $objUser;
+    private $objMike;
+    
+    public function __construct(Users $objUser, $objMike, Task $objTask)
+    {
+        $this->objTask = $objTask;
+        $this->objUser = $objUser;
+        $this->objMike = $objMike;
+            
+    }
     private $arrPlugins = array
     (
-        "EF_COMMENT" => "",
-        "EF_UPLOAD_DOCUMENT" => "",
-        "EF_COMPLETE_CASE" => "",
-        "EF_REASSIGN" => ""
+        "EF_COMMENT" => "sendComment",
+        "EF_UPLOAD_DOCUMENT" => "uploadDocument",
+        "EF_COMPLETE_CASE" => "completeCase",
+        "EF_REASSIGN" => "reassign",
+        "EF_START_CASE" => "startCase"
     );
     
     public function load($type)
@@ -17,6 +30,24 @@ class pluginRegistry
             return false;
         }
         
-        return $this->arrPlugins[$type];
+        $this->arrPlugins[$type]();
+        //return $this->arrPlugins[$type];
+    }
+    
+    public function sendComment()
+    {
+    }
+    
+    public function uploadDocument()
+    {
+    }
+    
+    public function startCase()
+    {
+    }
+    
+    
+    public function completeCase()
+    {
     }
 }
